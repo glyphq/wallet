f8068c34b8471ee01a864274a86e3a503bafd081
alez04 <alez2106x@gmail.com>
2026-05-20 05:24:43 +0300

    feat(updater): auto-updater hook, settings UI, signing in CI, updater manifest job

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,7 +19,6 @@ pub fn run() {
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_updater::Builder::default().build())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_process::init())
         .manage(AutoLockState::default())
         .manage(DeepLinkState::default())
         .manage(ClipboardState::default())
